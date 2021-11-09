be67a87f59cf367a5c47b536c6bc0db34aa7cf27
Thiago170 <91340096+Thiago170@users.noreply.github.com>
2021-11-09 09:03:54 -0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Speech.AudioFormat;
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+        bool canread = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_MouseEnter(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            canread = !canread;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (canread)
+            {
+                synth.Speak(label1.Text);
+            }
+        }
+    }
+}
